22397b5ff99dfb805d7dd63262811dca5a77e4a2
Mike Steel <michael.steel@smartpension.co.uk>
2023-09-11 13:53:47 +0100

    test

--- server/index.js
+++ server/index.js
@@ -12,7 +12,8 @@ const port = process.env.PORT || 8000
 app.use(express.static(path.join(__dirname, 'build')))
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+  res.send('Hello World!')
+  // res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
 // I'm maintaining all active connections in this object
