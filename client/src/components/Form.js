19953945828d48e32fa7fcfa016ac778d6890475
Mike Steel <michael.steel@smartpension.co.uk>
2023-09-20 07:19:19 +0100

    swaps to t shirt sizes

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,11 +1,18 @@
 import { VoteOption } from './VoteOption'
 
 export function Form({ username }) {
-  const voteOptions = ['1', '2', '3', '5', '8', '13', '20', '🤷', '☕️']
+  const voteOptions = ['XS', 'S', 'M', 'L', 'XL', '🤷', '☕️']
 
   return (
     <div>
       <h2>Enter your estimate below to vote</h2>
+      <ul>
+        <li>XS = less than 1 sprint</li>
+        <li>S = 1 sprint</li>
+        <li>M = 2 sprints</li>
+        <li>L = 3 sprints</li>
+        <li>XL = 4 sprints</li>
+      </ul>
       <div className="form">
         {voteOptions.map((option) => (
           <VoteOption
